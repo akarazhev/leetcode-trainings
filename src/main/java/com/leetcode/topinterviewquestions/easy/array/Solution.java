e9ce3ab73face0374002362f9462dfdde3aed398
Andrey Karazhev <karazhev@gmail.com>
2025-01-08 11:48:28 +0300

    Fixed "Remove Duplicates from Sorted Array" task.

--- src/main/java/com/leetcode/topinterviewquestions/easy/array/Solution.java
+++ src/main/java/com/leetcode/topinterviewquestions/easy/array/Solution.java
@@ -14,6 +14,14 @@ public class Solution {
      * - Return k.
      */
     public int removeDuplicates(int[] nums) {
-        return 0;
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
     }
 }
